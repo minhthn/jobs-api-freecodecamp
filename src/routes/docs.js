118690255ff5bf0211475f14acb9095204a068cc
minhthn <chemminh2k@gmail.com>
2023-07-03 16:08:44 +0700

    fix bug: vercel cannot read relative path './swagger.yaml'

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import parseYAML from '../utils/parseYAML';
+import path from 'node:path';
 
 const router = express.Router();
 
-const swaggerDocument = parseYAML('./swagger.yaml');
+const swaggerDocument = parseYAML(path.join(__dirname, '..', '..', 'swagger.yaml'));
 
 router.use('/', swaggerUI.serve);
 router.get('/', swaggerUI.setup(swaggerDocument));
